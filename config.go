af71eff59526fe4f98c378273f0fe71e8185e861
Harsh4902 <harshparmar4902@gmail.com>
2025-04-16 23:49:13 +0530

    Add suport for config.yaml file to save the context

---
+++ config.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Instance struct {
+		Name        string `yaml:"name"`
+		Image       string `yaml:"image"`
+		Status      string `yaml:"status"`
+		Port        string `yaml:"port"`
+		ContainerID string `yaml:"containerID"`
+		AutoRemove  bool   `yaml:"autoRemove"`
+	} `yaml:"instance"`
+}
+
+// Functions related to configs
+func defaultConfig() *Config {
+	return &Config{
+		Instance: struct {
+			Name        string `yaml:"name"`
+			Image       string `yaml:"image"`
+			Status      string `yaml:"status"`
+			Port        string `yaml:"port"`
+			ContainerID string `yaml:"containerID"`
+			AutoRemove  bool   `yaml:"autoRemove"`
+		}{
+			Name:       "microcks",
+			Image:      "",
+			Status:     "",
+			Port:       "",
+			AutoRemove: false,
+		},
+	}
+}
+
+func EnsureConfig(path string) (*Config, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		fmt.Println("Config not found. Initializing default config.")
+		cfg := defaultConfig()
+		err := SaveConfig(path, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create default config: %w", err)
+		}
+		return cfg, nil
+	}
+	return LoadConfig(path)
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func SaveConfig(path string, cfg *Config) error {
+	err := os.MkdirAll(filepath.Dir(path), 0755)
+	if err != nil {
+		return err
+	}
+	data, err := yaml.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module github.com/Harsh4902/Go-Docker
 
 go 1.24.1
 
-require github.com/docker/docker v28.0.2+incompatible
+require (
+	github.com/docker/docker v28.0.2+incompatible
+	github.com/docker/go-connections v0.5.0
+	gopkg.in/yaml.v2 v2.4.0
+)
 
 require (
 	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
-	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
